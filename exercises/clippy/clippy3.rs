b92e4cea26b5937a5fa58c7e9d8fbb9e611b8e96
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-04-10 23:21:04 +0100

    clippy

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -6,14 +6,10 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {
-        my_option.unwrap();
-    }
 
-    let my_arr = &[
-        -1, -2, -3
-        -4, -5, -6
-    ];
+    my_option.expect("test");
+
+    let my_arr = &[-1, -2, -3 - 4, -5, -6];
     println!("My array! Here it is: {:?}", my_arr);
 
     let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
