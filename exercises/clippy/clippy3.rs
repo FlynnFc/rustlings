71955cd84457aa1d900e0d403e4bc9e7045b7ac1
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-04-19 01:34:49 +0100

    .

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -1,21 +1,16 @@
 // clippy3.rs
 // Here's a couple more easy Clippy fixes, so you can see its utility.
 
-// I AM NOT DONE
-
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-
     my_option.expect("test");
-
     let my_arr = &[-1, -2, -3 - 4, -5, -6];
-    println!("My array! Here it is: {:?}", my_arr);
-
-    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
-    println!("This Vec is empty, see? {:?}", my_empty_vec);
+    println!("My array! Here it is: {my_arr:?}");
 
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
+    std::mem::swap(&mut value_a, &mut value_b);
+    println!("value a: {value_a}; value b: {value_b}");
 }
